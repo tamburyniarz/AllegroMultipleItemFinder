a37c9f2e74cd013e0b055cb2ca74be930f9edf39
DESKTOP-14FF54E\Senpai <tamburyniarz@gmail.com>
2016-08-16 14:29:50 +0200

    Added MainScreen

---
+++ core/src/com/mygdx/template/screens/MainScreen.java
@@ -0,0 +1,16 @@
+package com.mygdx.template.screens;
+
+/**
+ * Created by Senpai on 16.08.2016.
+ */
+public class MainScreen extends AbstractScreen {
+    @Override
+    public void buildStage() {
+        
+    }
+
+    @Override
+    public void initBgTexture() {
+
+    }
+}
